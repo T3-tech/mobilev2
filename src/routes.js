2bb288fc78b5b8fe9fde234ea7acb68997d4a9ea
gabrielwatanabe043 <gabrielpereira569@hotmail.com>
2023-09-12 21:55:46 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './pages/Home'
 
 import Login from './pages/Login';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 
 export default function Routes() {
     return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Login' options={{ headerShown: false }}>
+                <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
 
-        <Stack.Navigator>
-            <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+                <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
 
-            <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-        </Stack.Navigator>
+            </Stack.Navigator>
+
+
+        </NavigationContainer >
 
     )
 }
