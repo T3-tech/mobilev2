df39352e916233eb664af4c3a1565fe2355b3c6d
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-10-28 17:40:59 -0300

    primeiro get feito

--- src/pages/Agendamento/Listar/index.js
+++ src/pages/Agendamento/Listar/index.js
@@ -1,31 +1,81 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, StatusBar, Header, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { Button } from "@rneui/base";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import {
+    StyleSheet,
+    Text,
+    ActivityIndicator,
+    FlatList,
+    View,
+} from "react-native";
 
+export default (props) => {
+    const [agendamento, setAgendamento] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-export default props => (
+    const url = "http://localhost:7096/api/Agendamentos";
 
-    <SafeAreaView>
-        <View >
+    const getAgendamento = async () => {
+        try {
+            const response = await fetch(url);
+            console.log(
+                "🚀 ~ file: index.js:23 ~ getAgendamento ~ response",
+                response
+            );
 
-            <Text></Text>
-        </View>
+            const json = await response.json();
+            console.log("🚀 ~ file: index.js:28 ~ getAgendamento ~ json", json);
+            setAgendamento(json);
+        } catch (error) {
+            console.log(
+                "🚀 ~ file: index.js:32 ~ getAgendamento ~ console.log(error):",
+                console.log(error)
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    </SafeAreaView>
+    useEffect(() => {
+        getAgendamento();
+    }, []);
 
-
-)
-
+    return (
+        <>
+            <View>
+                {isLoading ? (
+                    <ActivityIndicator size={80} />
+                ) : (
+                    <FlatList
+                        data={agendamento}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <Text>
+                                {item.profissionalNome}
+                                {item.clienteNome}
+                                {item.servicoNome}
+                                {item.data}
+                                {item.statusNome}
+                            </Text>
+                        )}
+                    />
+                )}
+                <Button title={"Atualizar"} onPress={() => getAgendamento()} />
+            </View>
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: '#000',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 50
+        backgroundColor: "#000",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
     },
     texto: {
         color: "#fff",
-        fontSize: 20
-    }
-})
+        fontSize: 20,
+    },
+});
