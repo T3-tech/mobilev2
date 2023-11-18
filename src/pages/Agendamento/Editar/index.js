9dc55dda14f4683b127009baabe029f23642fc49
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-11-18 18:55:18 -0300

    Arumando o Regex (┬┬﹏┬┬)

--- src/pages/Agendamento/Editar/index.js
+++ src/pages/Agendamento/Editar/index.js
@@ -1,95 +1,103 @@
-import { View, Text, TextInput, StyleSheet } from "react-native"
-import { useEffect, useState } from "react"
-import SelectDropdown from "react-native-select-dropdown"
-import { Ionicons } from "@expo/vector-icons"
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import SelectDropdown from "react-native-select-dropdown";
+import { Ionicons } from "@expo/vector-icons";
 
-export default ({ route }) => {    
-    const { id, data, servicoNome, clienteNome, clienteId, servicoId ,statusId, navigator } = route.params
-    const [dataAgendamento, setDataAgendamento] = useState(data)
-    const [servico, setServico] = useState([])
-    const [idServico, setServicoId] = useState(servicoId)
-    const [cliente, setCliente] = useState([])
-    const [idCliente, setClienteId] = useState(clienteId)
-    let responsePut
+export default ({ route }) => {
+    const {
+        id,
+        data,
+        servicoNome,
+        clienteNome,
+        clienteId,
+        servicoId,
+        statusId,
+        navigator,
+    } = route.params;
+    const [dataAgendamento, setDataAgendamento] = useState(data);
+    const [servico, setServico] = useState([]);
+    const [idServico, setServicoId] = useState(servicoId);
+    const [cliente, setCliente] = useState([]);
+    const [idCliente, setClienteId] = useState(clienteId);
+    let responsePut;
 
     const getCliente = async () => {
         try {
             const response = await fetch(
                 "https://agendamento-api-dev-btxz.3.us-1.fl0.io/api/Clientes"
-                )
-            const json = await response.json()
-            setCliente(json)
-        } 
-        catch (error) {
+            );
+            const json = await response.json();
+            setCliente(json);
+        } catch (error) {
             console.error(
-                "🚀 ~ file: index.js:24 ~ getCliente ~ console.log(error):",
-            )
+                "🚀 ~ file: index.js:24 ~ getCliente ~ console.log(error):"
+            );
         }
-    }
+    };
 
     const getServico = async () => {
         try {
             const response = await fetch(
                 "https://agendamento-api-dev-btxz.3.us-1.fl0.io/api/Servicos"
-            )
-            const json = await response.json()
-            setServico(json)
+            );
+            const json = await response.json();
+            setServico(json);
         } catch (error) {
             console.error(
-                "🚀 ~ file: index.js:38 ~ getServico ~ console.log(error):",
+                "🚀 ~ file: index.js:38 ~ getServico ~ console.log(error):"
             );
         }
-    }
+    };
 
     json = JSON.stringify({
         data: dataAgendamento,
         clienteId: Number(idCliente),
         servicoId: Number(idServico),
         statusId: Number(statusId),
-    })
+    });
 
     const editarAgendamento = async () => {
         if (dataAgendamento === "" || idCliente === "" || idServico === "") {
-            alert("Por favor, preencha todos os campos.")
+            alert("Por favor, preencha todos os campos.");
         } else {
             if (!patern.test(dataAgendamento)) {
-                alert("Por favor, preencha a data no padrão dd-mm-aaaa hh:mm")
-                return
+                alert("Por favor, preencha a data no padrão dd-mm-aaaa hh:mm");
+                return;
             }
             try {
-            responsePut = await fetch(
-                `https://agendamento-api-dev-btxz.3.us-1.fl0.io/api/Agendamentos/${id}`,
-                {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: json,
-                }
-            );
+                responsePut = await fetch(
+                    `https://agendamento-api-dev-btxz.3.us-1.fl0.io/api/Agendamentos/${id}`,
+                    {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: json,
+                    }
+                );
             } catch (error) {
                 console.error(
                     "🚀 ~ file: index.js:65 ~ editAgendamento ~ console.log(error):"
-                )
+                );
             } finally {
                 if (responsePut.status === 400) {
-                    alert("Erro ao editar agendamento.")
-                    return
-                } 
-                alert("Agendamento editado com sucesso!")
-                navigator.navigate("ListarAgendamento")
+                    alert("Erro ao editar agendamento.");
+                    return;
+                }
+                alert("Agendamento editado com sucesso!");
+                navigator.navigate("ListarAgendamento");
             }
         }
-    }
+    };
 
-    const regex = `^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](202[0-9])[ ](0[0-9]|1[0-9]|2[0123])[:](0[0-9]|[60][0-9])`
+    const regex = `^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](202[0-9])[ ](0[0-9]|1[0-9]|2[0123])[:](0[0-9]|[59][0-9])`;
 
-    const patern = new RegExp(regex)
-    
+    const patern = new RegExp(regex);
+
     function validateServicoId(nomeServico) {
         servico.map((item) => {
             if (nomeServico.includes(item.nome)) {
-                setServicoId(item.id)
+                setServicoId(item.id);
             }
         });
     }
@@ -97,30 +105,31 @@ export default ({ route }) => {
     function validateClienteId(nomeCliente) {
         cliente.map((item) => {
             if (item.nome === nomeCliente) {
-                setClienteId(item.id)
+                setClienteId(item.id);
             }
         });
     }
 
     useEffect(() => {
-        getCliente()
-        getServico()
+        getCliente();
+        getServico();
     }, []);
 
     return (
         <>
             <View style={styles.container}>
-                <TextInput style={styles.input}
-                    placeholder='Data/Hora'
+                <TextInput
+                    style={styles.input}
+                    placeholder="Data/Hora"
                     value={dataAgendamento}
                     onChangeText={setDataAgendamento}
-                    placeholderTextColor={"#fff"}>
-                </TextInput>
+                    placeholderTextColor={"#fff"}
+                ></TextInput>
                 <Text>Padrão para data: dd-mm-aaaa hh:mm</Text>
                 <SelectDropdown
                     data={cliente.map((item) => item.nome)}
                     onSelect={(selectedItem, index) => {
-                        validateClienteId(selectedItem)
+                        validateClienteId(selectedItem);
                     }}
                     defaultButtonText={clienteNome}
                     searchPlaceHolder={"Pesquisar cliente"}
@@ -133,13 +142,15 @@ export default ({ route }) => {
                         borderRadius: 5,
                         backgroundColor: "#6E6E6E",
                     }}
-                    buttonTextStyle={{color: "#FFF"}}
+                    buttonTextStyle={{ color: "#FFF" }}
                     search={true}
                 />
                 <SelectDropdown
-                    data={servico.map((item) => item.nome + " - R$" + item.valor)}
+                    data={servico.map(
+                        (item) => item.nome + " - R$" + item.valor
+                    )}
                     onSelect={(selectedItem, index) => {
-                        validateServicoId(selectedItem)
+                        validateServicoId(selectedItem);
                     }}
                     defaultButtonText={servicoNome}
                     searchPlaceHolder={"Pesquisar serviço"}
@@ -152,7 +163,7 @@ export default ({ route }) => {
                         borderRadius: 5,
                         backgroundColor: "#6E6E6E",
                     }}
-                    buttonTextStyle={{color: "#FFF"}}
+                    buttonTextStyle={{ color: "#FFF" }}
                     search={true}
                 />
             </View>
@@ -165,8 +176,8 @@ export default ({ route }) => {
                 />
             </View>
         </>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -183,7 +194,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#6E6E6E",
         borderRadius: 5,
         justifyContent: "center",
-        textAlign:'center',
+        textAlign: "center",
         color: "#FFF",
         fontSize: 17,
     },
@@ -196,5 +207,5 @@ const styles = StyleSheet.create({
     textoInput: {
         fontSize: 15,
         fontWeight: "bold",
-    }
-})
+    },
+});
